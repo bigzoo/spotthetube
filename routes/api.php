c500884e57f7176a292cfa99ec57e5f1e6b6bc89
Chris Nyaga <nyagachris75@gmail.com>
2018-06-03 17:02:24 +0300

    Updated application api and web routes

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,17 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+/** @var \Dingo\Api\Routing\Router $api */
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1', function ($api) {
+    /** @var \Dingo\Api\Routing\Router $api */
+    $api->get('spotify', \App\Http\Controllers\Api\SpotifyController::class . '@index');
+    $api->get('cow', function(){
+         return response()->json(['beware' => 'I gorra thing for cows!']);
+    });
+});
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,21 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/**
+ * General Routes
+ */
+Route::get('/', 'HomeController@home')->name('home');
+Route::get('about', 'HomeController@about')->name('about');
+
+/**
+ * Spotify and Youtube Authentication Routes
+ */
+Route::get('auth/spotify','AuthController@spotifyAuth')->name('spotify.auth');
+Route::get('auth/youtube','AuthController@youtubeAuth')->name('youtube.auth');
+
+
+/**
+ * Spotify and Youtube Authentication Callback Routes
+ */
+Route::get('callback/spotify','AuthController@spotifyCallback')->name('spotify.callback');
+Route::get('callback/youtube','AuthController@youtubeCallback')->name('youtube.callback');
