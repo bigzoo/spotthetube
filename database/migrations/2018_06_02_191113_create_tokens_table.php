e3cbf6adae83094b053e5eeb27f37e50400e7cc1
Chris Nyaga <nyagachris75@gmail.com>
2018-06-03 17:01:34 +0300

    updated migrations

--- database/migrations/2018_06_02_191113_create_tokens_table.php
+++ database/migrations/2018_06_02_191113_create_tokens_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateTokensTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tokens', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->text('access_token');
+            $table->text('session_token');
+            $table->text('provider');
+            $table->text('refresh_token')->nullable();
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tokens');
     }
 }
