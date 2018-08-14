cafbd608e90ba3d6250122f26b8792c85727a95b
Chris Nyaga <nyagachris75@gmail.com>
2018-08-14 19:58:11 +0300

    - Updated db config for heroku - Compiled assets for production

--- config/database.php
+++ config/database.php
@@ -1,9 +1,12 @@
 <?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
+$isProd = env('APP_ENV') == 'production';
+if ($isProd) {
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
+}
 return [
 
     /*
@@ -45,17 +48,25 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => $host,
+            'host' => $isProd ? $host : env('DB_HOST'),
             'port' => env('DB_PORT', '3306'),
-            'database' => $database,
-            'username' => $username,
-            'password' => $password,
+            'database' => $isProd ? $database : env('DB_DATABASE'),
+            'username' => $isProd ? $username : env('DB_USERNAME'),
+            'password' => $isProd ? $password : env('DB_PASSWORD'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'strict' => true,
             'engine' => null,
+            'modes' => [
+                'ONLY_FULL_GROUP_BY',
+                'STRICT_TRANS_TABLES',
+                'NO_ZERO_IN_DATE',
+                'NO_ZERO_DATE',
+                'ERROR_FOR_DIVISION_BY_ZERO',
+                'NO_ENGINE_SUBSTITUTION',
+            ],
         ],
 
         'pgsql' => [
